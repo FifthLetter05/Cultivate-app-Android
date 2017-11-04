35d87b79f9110bc02be28e852664206ca36f6105
Ian Kraybill <iek2day@gmail.com>
2017-11-03 22:00:29 -0700

    changed logo

--- app/src/main/java/coop/cultivatecommunity/cultivate/LaunchActivity.kt
+++ app/src/main/java/coop/cultivatecommunity/cultivate/LaunchActivity.kt
@@ -3,13 +3,16 @@ package coop.cultivatecommunity.cultivate
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
 
 class LaunchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        var logo = ImageView(this)
 
+        logo.setImageURI()
 
         intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
