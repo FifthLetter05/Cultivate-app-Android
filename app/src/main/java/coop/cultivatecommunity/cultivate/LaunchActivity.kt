c86747299a3175d8a18f0f3ee5810c5a51bef07f
Ian Kraybill <iek2day@gmail.com>
2017-11-03 22:43:24 -0700

    worked on launcher

--- app/src/main/java/coop/cultivatecommunity/cultivate/LaunchActivity.kt
+++ app/src/main/java/coop/cultivatecommunity/cultivate/LaunchActivity.kt
@@ -1,9 +1,11 @@
 package coop.cultivatecommunity.cultivate
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
+import java.io.File
 
 class LaunchActivity : AppCompatActivity() {
 
@@ -12,7 +14,7 @@ class LaunchActivity : AppCompatActivity() {
 
         var logo = ImageView(this)
 
-        logo.setImageURI()
+        logo.setImageURI(Uri.fromFile(File("/assets/logo.png")))
 
         intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
